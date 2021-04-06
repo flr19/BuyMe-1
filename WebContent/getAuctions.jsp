242b7702ef47e22e48230c22c0f6c2c5f5861114
kuhuhalder <kuhuhalder2701@gmail.com>
2021-04-06 11:46:35 -0400

    updated sql

--- WebContent/getAuctions.jsp
+++ WebContent/getAuctions.jsp
@@ -1,73 +1,95 @@
-<%-- <%@ page language="java" contentType="text/html; charset=ISO-8859-1" pageEncoding="ISO-8859-1"%>
+<%-- 
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+	pageEncoding="ISO-8859-1"%>
 <%@ page import="java.io.*,java.util.*,java.sql.*,java.text.*"%>
-<%@ page import="javax.servlet.http.*,javax.servlet.*"%> 
+<%@ page import="javax.servlet.http.*,javax.servlet.*"%>
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8" import="com.cs336.pkg.*"%>
-	
+	pageEncoding="UTF-8" import="com.cs336.pkg.*"%>
+
 <!DOCTYPE html>
 <html>
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>BuyMe</title>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>BuyMe</title>
 </head>
-<body> 	
- 	
- 	<% if (session.getAttribute("user") == null) { 
-    		response.sendRedirect("login.jsp");
-       } else { %>
-    	<div class="content">
-    	<%
-    	Connection conn = null;
-			Statement s = null;
-    		ResultSet rs = null;
-    		 ApplicationDB db = new ApplicationDB();	
-    			conn = db.getConnection(); 	
-    		try {
-				Locale locale = new Locale("en", "US");
-				NumberFormat currency = NumberFormat.getCurrencyInstance(locale);
-				String allAuctionsQuery = "SELECT * FROM product WHERE sold=false";
-				s = conn.createStatement();
-				rs = s.executeQuery(allAuctionsQuery);
-				if (rs.next()) { %>
-					<h2>All Live Auctions</h2>
-					<table>
-						<tr>
-							<th>Item</th>
-							<th>Seller</th>
-							<th>Current Bid</th>
-							<th>End Date/Time</th>
-						</tr>
-						<%	do { %>
-						<tr>
-							<td>
-								<a href="auction.jsp?productId=<%= rs.getInt("productid") %>">
-									<%= rs.getString("brand")/*  + " " + rs.getString("model") + " " + rs.getString("gender") +  " " + rs.getFloat("size") */ %>
-								</a>
-							</td>
-							<td><%= rs.getString("seller") %></td>
-							<td><%= currency.format(rs.getDouble("price")) %></td>
-							<td><%= rs.getString("endDate") %></td>
-						</tr>
-				 <%		} while (rs.next()); %> 
-					</table>
-				<%	} else { %>
-						<br><h3>There are currently no live auctions.</h3>
-				<%	} %>		
-			<%	
-			
-				
-				
-    		} catch (SQLException e){
-    			out.print("<p>Error connecting to MYSQL server.</p>");
-			    e.printStackTrace();    			
-    		} finally {
-				try { rs.close(); } catch (Exception e) {} 
-				try { s.close(); } catch (Exception e) {} 
-				try { conn.close(); } catch (Exception e) {} 
-    		}   	
-    	%>
-    	</div>
-    <% } %>
+<body>
+
+	<%
+	if (session.getAttribute("user") == null) {
+		response.sendRedirect("login.jsp");
+	} else {
+	%>
+	<div class="content">
+		<%
+		Connection conn = null;
+		Statement s = null;
+		ResultSet rs = null;
+		ApplicationDB db = new ApplicationDB();
+		conn = db.getConnection();
+		try {
+			Locale locale = new Locale("en", "US");
+			NumberFormat currency = NumberFormat.getCurrencyInstance(locale);
+			String allAuctionsQuery = "SELECT * FROM product WHERE sold=false";
+			s = conn.createStatement();
+			rs = s.executeQuery(allAuctionsQuery);
+			if (rs.next()) {
+		%>
+		<h2>All Live Auctions</h2>
+		<table>
+			<tr>
+				<th>Item</th>
+				<th>Seller</th>
+				<th>Current Bid</th>
+				<th>End Date/Time</th> a
+			</tr>
+			<%
+			do {
+			%>
+			<tr>
+				<td><a href='auction.jsp?productId=<%=rs.getInt("productid")%>'>
+						<%=rs.getString(
+		"brand")/*  + " " + rs.getString("model") + " " + rs.getString("gender") +  " " + rs.getFloat("size") */%>
+				</a></td>
+				<td><%=rs.getString("seller")%></td>
+				<td><%=currency.format(rs.getDouble("price"))%></td>
+				<td><%=rs.getString("endDate")%></td>
+			</tr>
+			<%
+			} while (rs.next());
+			%>
+		</table>
+		<%
+		} else {
+		%>
+		<br>
+		<h3>There are currently no live auctions.</h3>
+		<%
+		}
+		%>
+		<%
+		} catch (SQLException e) {
+		out.print("<p>Error connecting to MYSQL server.</p>");
+		e.printStackTrace();
+		} finally {
+		try {
+			rs.close();
+		} catch (Exception e) {
+		}
+		try {
+			s.close();
+		} catch (Exception e) {
+		}
+		try {
+			conn.close();
+		} catch (Exception e) {
+		}
+		}
+		%>
+	</div>
+	<%
+	}
+	%>
 </body>
-</html> --%>
+</html>
+ --%>
--- auctionsystem.sql
+++ auctionsystem.sql
@@ -1,74 +1,117 @@
-
-DROP DATABASE `auctionsystem`;
-CREATE DATABASE IF NOT EXISTS `AuctionSystem`;
-USE AuctionSystem;
+DROP DATABASE auctionsystem;
+CREATE DATABASE IF NOT EXISTS auctionsystem;
+USE auctionsystem;
 
 CREATE TABLE account(
-`username` varchar(30) 	NOT NULL,
-`password` varchar(30) 	NOT NULL,
-`address` varchar(30) NOT NULL,
-`email` varchar(30) NOT NULL,
-`name` varchar(50) NOT NULL,
-primary key(`username`));
-INSERT INTO `account` VALUES ('kuhu27' , 'halder', '1202', 'kuhu@com', 'kuhu'), ('farah01' , 'lubaba', 'saudi', 'farah@com', 'farah'),('jigisha19' , 'mavani', 'edison', 'jigisha@com', 'jigisha'), ('prince18', '1234', 'newbrunswick', 'prince@com', 'prince');
+username varchar(30) 	NOT NULL,
+password varchar(30) 	NOT NULL,
+address varchar(30) NOT NULL,
+email varchar(30) NOT NULL,
+name varchar(50) NOT NULL,
+primary key(username));
+INSERT INTO account VALUES ('kuhu27' , 'halder', '1202', 'kuhu@com', 'kuhu'), ('pink' , 'pink', '1202', 'pink@com', 'pink'), ('farah01' , 'lubaba', 'saudi', 'farah@com', 'farah'),('jigisha19' , 'mavani', 'edison', 'jigisha@com', 'jigisha'), ('prince18', '1234', 'newbrunswick', 'prince@com', 'prince');
 
 
 CREATE TABLE product(
-`productid` int NOT NULL,
-`minprice` int 	NOT NULL,
-`seller` varchar(30) 	NOT NULL,
-`price` int NOT NULL,
-`category` varchar(30) NOT NULL,
-`currentbid` int NOT NULL,
-primary key(`productid`));
+	product_id INT AUTO_INCREMENT,
+	category VARCHAR(50) NOT NULL,
+	brand VARCHAR(25) NOT NULL,
+    color VARCHAR(20),
+    gender VARCHAR(20),
+	seller VARCHAR(50) NOT NULL,
+    min_price DECIMAL (20,2) NOT NULL,
+	price DECIMAL(20,2) NOT NULL,
+	status BOOLEAN,
+    start_date DATETIME NOT NULL,
+    end_date DATETIME NOT NULL,
+    winner VARCHAR(20),
+	PRIMARY KEY(product_id),
+    foreign key(seller) references account(username)
+);
 
 CREATE TABLE bid(
+bid_id INT AUTO_INCREMENT,
 username varchar(50) NOT NULL, 
-upperlimit int NOT NULL, 
-bidHistory varchar(20) NOT NULL, 
-bidIncremen int NOT NULL,
-productid int NOT NULL, 
-primary key(username, productid),
+upper_limit int NOT NULL, 
+-- bidHistory varchar(20) NOT NULL, 
+bid_increment int NOT NULL,
+amount DECIMAL (20,2) NOT NULL,
+product_id int NOT NULL, 
+primary key(bid_id, username, product_id),
 foreign key(username) references account(username),
-foreign key(productid) references product(productid));
+foreign key(product_id) references product(product_id));
 
-CREATE TABLE alert(
-`username` varchar(50) NOT NULL, 
-`userbid` int NOT NULL, 
-`productid` int NOT NULL, 
-primary key(`username`, `productid`),
-foreign key(`username`) references account(username),
-foreign key(`productid`) references product(productid));
+CREATE TABLE alerts(
+alert_id INT AUTO_INCREMENT,
+username varchar(50) NOT NULL, 
+alert_message varchar(50),
+user_bid int NOT NULL, 
+product_id int NOT NULL, 
+primary key(alert_id, username, product_id),
+foreign key(username) references account(username),
+foreign key(product_id) references product(product_id));
 
-CREATE TABLE sell(
-`username` varchar(50) NOT NULL, 
-`reserve` int NOT NULL, 
-`enddate` int NOT NULL, 
-`endtime` int NOT NULL, 
-`productid` int NOT NULL,
-primary key(`productid`),
-foreign key(`username`) references account(username),
-foreign key(`productid`) references product(productid));
+CREATE TABLE questions(
+	question_id INT AUTO_INCREMENT,
+    username VARCHAR(50),
+    question VARCHAR(250) NOT NULL,
+    answer VARCHAR(250) DEFAULT NULL,
+    FOREIGN KEY (username) REFERENCES account(username),
+		-- ON DELETE CASCADE,
+    PRIMARY KEY (question_id)
+);
+-- CREATE TABLE sell(
+-- username varchar(50) NOT NULL, 
+-- reserve int NOT NULL, 
+-- end_date int NOT NULL, 
+-- endt_ime int NOT NULL, 
+-- product_id int NOT NULL,
+-- primary key(product_id),
+-- foreign key(username) references account(username),
+-- foreign key(product_id) references product(product_id));
 
 CREATE TABLE users(
-`username` varchar(30) 	NOT NULL,
-`password` varchar(30) 	NOT NULL,
-primary key(`username`),
-foreign key(`username`) references account(username));
+username varchar(30) 	NOT NULL,
+password varchar(30) 	NOT NULL,
+primary key(username),
+foreign key(username) references account(username)
+-- foreign key(password) references account(password)
+);
+
+insert into users values('pink', 'pink');
+
+CREATE TABLE buyers(
+username varchar(30) 	NOT NULL,
+password varchar(30) 	NOT NULL,
+primary key(username),
+foreign key(username) references users(username)
+-- foreign key(password) references users(password)
+);
+
+CREATE TABLE sellers(
+username varchar(30) 	NOT NULL,
+password varchar(30) 	NOT NULL,
+primary key(username),
+foreign key(username) references users(username)
+-- foreign key(password) references users(password)
+);
 
 CREATE TABLE customerrep(
-`username` varchar(30) 	NOT NULL,
-`password` varchar(30) 	NOT NULL,
-primary key(`username`),
-foreign key(`username`) references account(username));
+username varchar(30) 	NOT NULL,
+password varchar(30) 	NOT NULL,
+primary key(username),
+foreign key(username) references account(username)
+-- foreign key(password) references account(password)
+);
 
-INSERT INTO `customerrep` VALUES ('kuhu27' , 'halder'), ('farah01' , 'lubaba'),('jigisha19' , 'mavani');
+INSERT INTO customerrep VALUES ('kuhu27' , 'halder'), ('farah01' , 'lubaba'),('jigisha19' , 'mavani');
 
 CREATE TABLE admin(
-`username` varchar(30) 	NOT NULL,
-`password` varchar(30) 	NOT NULL,
-primary key(`username`),
-foreign key(`username`) references account(username));
+username varchar(30) 	NOT NULL,
+password varchar(30) 	NOT NULL,
+primary key(username),
+foreign key(username) references account(username)
+-- foreign key(password) references account(password)
+);
 
-INSERT INTO `admin` VALUES ('prince18' , '1234');
-
+INSERT INTO admin VALUES ('prince18' , '1234');
